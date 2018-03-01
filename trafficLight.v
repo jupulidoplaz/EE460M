18992f13966a1d6e372ef237096b81742eac0e2e
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-03-01 13:06:22 -0600

    code that compiles!

--- trafficLight.v
+++ trafficLight.v
@@ -11,22 +11,27 @@ State = 0;
 end
 
 always @(RST)
-if (RST == 1)
-Counter = 0;
-State = 6;
-else
-State = 0;
+begin
+	if (RST == 1)
+	begin
+		Counter <= 0;
+		State <= 6;
+	end
+	else
+	State = 0;
 end
 
 
 always @(posedge CLK)
 begin
-Counter = Counter + 1;
-if(State == 5)
-Rw = ~Rw;
-if (Counter == FinishTime)
-	State = NextState;
-	Counter = 0;
+	Counter = Counter + 1;
+	if(State == 5)
+		Rw = ~Rw;
+	if (Counter == FinishTime)
+		begin
+			State = NextState;
+			Counter = 0;
+		end
 end
 
 
@@ -36,57 +41,59 @@ begin
 	case (State)
 	0:	
 		begin
-			NextState = 1;
-			FinishTime = 6;
-			Ra = 0;
-			{Ga, Rb, Rw} = 3'b111;
+			NextState <= 1;
+			FinishTime <= 6;
+			Ra <= 0;
+			{Ga, Rb, Rw} <= 3'b111;
 		end
 
 	1:
 		begin
-			NextState = 2;
-			FinishTime = 4;
-			Ga = 0;
+			NextState <= 2;
+			FinishTime <= 4;
+			Ga <= 0;
 		end
 
 	2:
 		begin
-			NextState = 3;
-			FinishTime = 6;
-			{Ya, Rb} = 2'b00;
-			{Ra, Gb} = 2'b11;
+			NextState <= 3;
+			FinishTime <= 6;
+			{Ya, Rb} <= 2'b00;
+			{Ra, Gb} <= 2'b11;
 		end
 
 	3:
 		begin
-			NextState = 4;
-			FinishTime = 2;
-			Gb = 0;
-			Yb = 1;
+			NextState <= 4;
+			FinishTime <= 2;
+			Gb <= 0;
+			Yb <= 1;
 		end
 
 	4:
 		begin
-			NextState = 5;
-			FinishTime = 4;
-			{Yb, Rw} = 2'b00;
-			{Rb, Gw} = 2'b11;
+			NextState <= 5;
+			FinishTime <= 4;
+			{Yb, Rw} <= 2'b00;
+			{Rb, Gw} <= 2'b11;
 		end
 
 	5:
 		begin
-			NextState = 0;
-			FinishTime = 4;
-			Gw = 0;
-			Rw = 1;
+			NextState <= 0;
+			FinishTime <= 4;
+			Gw <= 0;
+			Rw <= 1;
 		end
 
 	6:
 		begin
-			NextState = 6;
-			FinishTime = 2;
-			{Ya, Ga, Yb, Ga, Gw} = 5'b00000;
-			{Ra, Rb, Rw} = ~{Ra, Rb, Rw};
+			NextState <= 6;
+			FinishTime <= 2;
+			{Ya, Ga, Yb, Ga, Gw} <= 5'b00000;
+			{Ra, Rb, Rw} <= ~{Ra, Rb, Rw};
 		end
 	endcase
 end
+
+endmodule
