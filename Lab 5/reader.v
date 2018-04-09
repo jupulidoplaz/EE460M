e873f947eea33f48ff39f3615b69c503af595612
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-09 18:55:42 -0500

    Update reader.v

--- Lab 5/reader.v
+++ Lab 5/reader.v
@@ -1,43 +1,39 @@
-module reader(ps2CLK, ps2DATA, scanCode, idle, flash, register, LED2);
+module reader(ps2CLK, ps2DATA, scanCode, flash, idle);
     
 input ps2CLK, ps2DATA;
-output reg idle, flash, LED2;
+output wire flash;
+output reg idle;
 output reg[7:0] scanCode;
-output[7:0] register;
-reg[7:0] pastCode;
 reg [21:0] shiftReg;	
 
-    
-    
+assign flash = (shiftReg[8:1] == 8'hF0) ? 0 : 1;
+
 initial 
 begin
-idle = 1;
-flash = 0;
-pastCode = 0;
-LED2 = 0;
+    idle = 1;
+    shiftReg = 0;
+    scanCode = 0;
 end
 
-assign register = shiftReg[8:1];
 
 always @(negedge ps2CLK)
 begin
-    shiftReg = {ps2DATA, shiftReg[21:1]};	//right shifts data every negative edge of ps2 clock
+    shiftReg = {ps2DATA, shiftReg[21:1]};
+end
+
+always @(shiftReg)
+begin
     if (shiftReg[8:1] == 8'hF0)
     begin
         scanCode = shiftReg[19:12];
-        idle <= 0;
-        flash <= 1;
-        shiftReg[8:1] = 8'b00000000;
-       // shiftReg = 0;
+        idle = 0;
     end
     else
     begin
-        flash <= 0;
-        scanCode <= 0;
-        idle <= 1;
-        shiftReg <= shiftReg;
+        scanCode = scanCode;
+        idle = 1;
     end
 end
-	
+
 	
 endmodule
