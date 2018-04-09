55a6887bc98e61dddb830497cebafc8b901f7220
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-09 18:59:14 -0500

    Update strobe.v

--- Lab 5/strobe.v
+++ Lab 5/strobe.v
@@ -1,71 +1,31 @@
-`define endflash 10000000 //should be 10000000
+`define endflash 100 //should be 10000000
 module strobe(CLK, flash, LED);
 
 input CLK, flash;
 output reg LED; 
 reg[23:0] counter;
-reg lock, state;
+
 
 initial
 begin
     LED = 0;
     counter = 0;
-    lock = 0;
-    state = 0;
 end
     
 
 
-always@(posedge CLK)
+always@(negedge flash)
 begin
-    if(flash && lock)
+    if (counter < `endflash)
     begin
-        state = 1;
-        lock = 1;
+        counter = counter + 1;
+        LED = 1;
     end
-    
-    else if(flash && !lock)
-    begin
-        state = 1;
-        lock = 1;
-    end
-    
-    else if(!flash && lock)
-    begin
-        state = 1;
-        lock = 1;
-    end
-    
     else
     begin
-        state = 0;
-        lock = 0;
+        counter = 0;
+        LED = 0;
     end
-        
-    case(state)
-    0:  begin
-            LED <=0;
-            counter <=0;
-            //state <= 0;
-        end
-    1:  begin
-            if(counter >= `endflash)
-            begin
-                LED <= 0;
-                counter <= 0;
-                lock <= 0;
-                //state <= 0;
-            end
-            else
-            begin
-                LED <= 1;
-                counter <= counter +1;
-                //state <= 1;
-            end
-        
-        end
-    endcase
-    
 end
     
 endmodule
