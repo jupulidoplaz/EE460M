21854c7a658a13c2d3ee09f9bd695289a772add2
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-19 15:36:26 -0500

    Create stepCount.v

---
+++ Lab 6/stepCount.v
@@ -0,0 +1,17 @@
+module stepCount(CLK, totalSteps, stepCount);
+input CLK;
+input [20:0] totalSteps;
+output reg [13:0] stepCount;
+output reg SI;
+
+always @(posedge CLK)
+begin
+if (totalSteps <= 9998)
+    stepCounter = totalSteps;
+    SI = 0;
+else
+    stepCounter = 9999;
+    SI = 1;
+end
+
+endmodule
