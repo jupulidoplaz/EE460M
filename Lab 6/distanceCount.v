f9b70c010edda81d44c2fb2bdb51f6e7a56faae4
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-19 16:21:51 -0500

    Update distanceCount.v

--- Lab 6/distanceCount.v
+++ Lab 6/distanceCount.v
@@ -1,18 +1,41 @@
-module distanceCount(CLK, totalSteps, distWhole, distFrac);
-input CLK;
+module distanceCount(CLK, steps, distWhole, distFrac);
+input CLK, steps;       //step signal from pulse gen
 input [20:0] totalSteps;
 output [20:0] distance;
 output [3:0] distFrac;
 output [6:0] distWhole; //up to 127 miles (7 bits)
 
-always @(posedge CLK)    
+always @(posedge CLK)   
 begin
-    distWhole = totalSteps / 2048;
-    if((totalSteps % 2048) < 1024)
-        distFrac = 0;
-    else
-        distFrac = 5;
+    if(increment)
+    begin
+        if(distFrac == 0)
+        begin
+            distFrac = 5;
+            distWhole = distWhole;
+        end
+        else
+        begin
+            distFrac = 0;
+            distWhole = distWhole + 1;
+        end            
+    end
+    
 end
-    
-    
+
+    always@(posedge steps)
+begin
+    counter = counter + 1;
+    if(counter == 1024)
+    begin
+        increment <= 1;
+        counter <= 0;
+    end
+    else
+    begin
+        increment <= 0;
+        counter <= counter;
+    end    
+end
 endmodule
+    
