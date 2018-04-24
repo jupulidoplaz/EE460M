c5257b872a87ccd50978e168d7198db8dad2e0ba
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 03:48:06 -0500

    Update complexDivider.v

--- Lab 6/complexDivider.v
+++ Lab 6/complexDivider.v
@@ -1,30 +1,30 @@
 module complexDivider(clk100Mhz, slowClk, slowEdge, enable);
-  input clk100Mhz, enable; //fast clock
-  output reg slowClk; //slow clock
-  input [27:0] slowEdge; //2*slowEdge times slower thn 10 MHz
-  reg[27:0] counter;
+input clk100Mhz, enable; //fast clock
+output reg slowClk; //slow clock
+input [27:0] slowEdge; //2*slowEdge times slower thn 10 MHz
 
-  initial begin
-    counter = 0;
-    slowClk = 0;
-  end
+reg[27:0] counter;
 
-  always @ (posedge clk100Mhz)
-  begin
-    if(enable)  begin
-        if(counter >= slowEdge) begin
-          counter <= 1;
-          slowClk <= ~slowClk;
-        end
-        else begin
-          counter <= counter + 1;
-        end
+initial begin
+counter = 0;
+slowClk = 0;
+end
+
+always @ (posedge clk100Mhz)
+begin
+if(enable)  begin
+    if(counter == slowEdge) begin
+      counter <= 1;
+      slowClk <= ~slowClk;
     end
     else begin
-        counter <= 0;
-        slowClk <= 0;
+      counter <= counter + 1;
     end
-    
-  end
+end
+else begin
+    counter <= 0;
+    slowClk <= 0;
+end
+end
 
 endmodule
