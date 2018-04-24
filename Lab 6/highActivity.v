48e0517ca7f73e01e139a2bad400768552afc321
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 03:49:54 -0500

    Update highActivity.v

--- Lab 6/highActivity.v
+++ Lab 6/highActivity.v
@@ -1,68 +1,67 @@
-module highActivity(CLK, steps, secClk, secs);
-input CLK, steps, secClk; //steps is step signal from pulse generator
-output [13:0] secs;
-reg [5:0] secCount;
-reg minFlag;
+module highActivity(clk, pulse, secClk, secs, reset);
+input clk, pulse, reset, secClk; //pulse is step signal from pulse generator
+output reg [8:0] secs; //up to 8.5 mins
+reg [7:0] stepCount; //up to 255 steps/sec
+reg [8:0] secCount; //amount of seconds that steps has been >= 64 per minute
+reg lock, secFlag, oldSecFlag; 
 
-    assign secs = (enable)			// idk what this is
-    
-initial 
+initial
 begin
-    minFlag = 0;
+    secs = 0;
+    stepCount = 0;
     secCount = 0;
-	stepReset = 0;
+    lock = 0;
+    secFlag = 0;
+end
+
+always@(posedge pulse)begin
+    if(!lock) begin 
+        if(oldSecFlag == secFlag) begin
+            stepCount <= stepCount + 1;
+        end
+        else    begin
+            stepCount <= 1;
+        end
+        
+        oldSecFlag <= secFlag;    //to make sure to clr stepCount every new second
+    end
+    else    begin
+        stepCount <= 0;
+        oldSecFlag <= secFlag;
+    end
+end
+
+
+always @(posedge secClk or posedge reset) begin
+    if (reset)  begin
+        secCount <= 0;
+        secs <= 0; 
+        lock <= 1;
+    end
+    else    begin
+        lock <= 0;
+        secFlag = ~secFlag;
+        
+        if(stepCount < 64)  begin
+            secCount <= 0;
+            secs <= secs;
+        end
+        
+        else    begin
+            if(secs < 60)   begin      
+                secCount <= secCount + 1;
+                if(secCount == 60)
+                    secs <= 60;
+                else
+                    secs <= secs;
+            end
+            else    begin
+                secs <= secs +1;
+                secCount <= 0;
+            end
+        end
+    end
 end
     
-always @(posedge secClk, reset)
-begin
-	secCount = secCount + 1;
-	if (reset)			// reset
-	begin
-		secs = 0;
-		secCount = 0;
-		minFlag = 0;
-		stepReset = 0;
-	end
-	else if ((secCount == 60) && (steps >= 64))	// if 60 steps with high activity, add 60 to display
-	begin
-		secs = secs + 60;
-		secCount = secCount;
-	end
-	else if (secCount > 60)				// display after 60 seconds
-	begin
-		if (steps >= 64)
-		begin
-			secs = secs + 1;
-			secCount = 61;				// use 61 to avoid previous if statement
-		end
-		else
-		begin
-			secs = secs;
-			secCount = 0;
-		end
-		minFlag = 1;
-	end
-	else if (steps >= 64)				// continue
-	begin
-		secs = secs;
-		secCount = secCount;
-	end
-	else						// too few steps before 60 seconds
-	begin
-		secs = secs;
-		secCount = 0;
-	end
-
-	stepReset = 1;
-	stepReset = 0;					// to trigger resetting of steps
-	minFlag = 0;
-end
-    
-
-always@(posedge steps, reset, posedge stepReset)
-begin
-	if (reset | stepReset) steps = 0;
-	else steps = steps + 1;
-end
-
 endmodule
+    
