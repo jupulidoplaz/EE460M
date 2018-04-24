3cb24bdb1124a262ba1a48a740b4295c6d5c448e
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 12:46:40 -0500

    Update distanceCount.v

--- Lab 6/distanceCount.v
+++ Lab 6/distanceCount.v
@@ -3,7 +3,7 @@ input clk, pulse, reset;
 output reg [6:0] whole;
 output reg [2:0] frac;
 reg [10:0] pulseCount;
-reg increment, done, lock;
+reg increment, done;
 
 initial
 begin
@@ -12,7 +12,7 @@ begin
     pulseCount = 0;
     increment = 0;
     done = 0;
-    lock = 0;
+   // lock = 0;
 end
 
 always @(posedge clk or posedge reset or negedge reset)
@@ -20,10 +20,10 @@ begin
     if (reset) begin
         whole <= 0;
         frac <= 0;
-        lock <= 1;
+      //  lock <= 1;
     end
     else if (increment && !done) begin  //done makes sure it only increments once
-        lock <= 0;                      //lock makes sure that it starts back up again when reset is deasserted
+      //  lock <= 0;                      //lock makes sure that it starts back up again when reset is deasserted
         done <= 1;
         if(!frac)   begin
             frac <= 5;
@@ -35,13 +35,13 @@ begin
         end 
     end
     else if(!increment) begin
-        lock <= 0;
+     //   lock <= 0;
         done <= 0;
         frac <= frac;
         whole <= whole;
     end   
     else    begin
-        lock <= 0;
+      //  lock <= 0;
         done <= done;
         frac <= frac;
         whole <= whole;
@@ -50,7 +50,7 @@ end
 
 always @(posedge pulse)
 begin
-    if (lock)   begin
+    if (reset)   begin
         pulseCount <= 0;
         increment <= 0;
     end
