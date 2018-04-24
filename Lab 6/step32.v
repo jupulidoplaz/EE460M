4f2848416835fd027f8f59dd8beef57257413cd1
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 12:44:48 -0500

    Update step32.v

--- Lab 6/step32.v
+++ Lab 6/step32.v
@@ -2,11 +2,11 @@ module step32(CLK, pulse, secClk, secs, reset); //pulse is the pulse signal
 input CLK, secClk, pulse, reset; 
 output reg [3:0] secs;
 reg [3:0] secCount;
-reg lock, secFlag, oldSecFlag;
+reg secFlag, oldSecFlag;
 reg [7:0] stepCount;
 initial
 begin
-    lock = 0;
+   // lock = 0;
     secFlag = 0;
     oldSecFlag = 0;
     secs = 0;
@@ -15,28 +15,31 @@ end
     
 always@(posedge pulse) 
 begin
-    if (!lock)  begin
+ //   if (!lock)  begin
+    if (!reset) begin
         if(oldSecFlag == secFlag)  
             stepCount <= stepCount + 1;
         else
             stepCount <= 1;
-        oldSecFlag <= secFlag;
+       // oldSecFlag <= secFlag;
     end
     else    
         stepCount <= 0;
+    oldSecFlag <= secFlag;
 end
     
 always @(posedge secClk or posedge reset)
 begin
+    secFlag <= ~secFlag;
     if (reset)  begin
-        lock <= 1;
-        secFlag <= secFlag;
+       // lock <= 1;
+      //  secFlag <= secFlag;
         secCount <= 0;
         secs <= 0;
     end
     else    begin
-        lock <= 0;
-        secFlag = ~secFlag;
+       // lock <= 0;
+      //  secFlag <= ~secFlag;
         secCount = secCount + 1;
         if (secCount <= 9)  begin    
             if(stepCount >= 32)
