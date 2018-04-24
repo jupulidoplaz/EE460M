7da86e06028e597eb0ede23fe357c63783a408fb
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 12:46:01 -0500

    Update highActivity.v

--- Lab 6/highActivity.v
+++ Lab 6/highActivity.v
@@ -3,19 +3,28 @@ input clk, pulse, reset, secClk; //pulse is step signal from pulse generator
 output reg [8:0] secs; //up to 8.5 mins
 reg [7:0] stepCount; //up to 255 steps/sec
 reg [8:0] secCount; //amount of seconds that steps has been >= 64 per minute
-reg lock, secFlag, oldSecFlag; 
+reg secFlag, oldSecFlag, oldPulse, pauseSec;
 
 initial
 begin
     secs = 0;
     stepCount = 0;
     secCount = 0;
-    lock = 0;
+   // lock = 0;
     secFlag = 0;
 end
 
-always@(posedge pulse)begin
-    if(!lock) begin 
+always @(posedge clk)
+begin
+    if(pulse == oldPulse)
+        pauseSec <= 1;
+    else
+        pauseSec <= 0;
+    oldPulse = pulse;    
+end
+
+always@(posedge pulse)begin         //when pulses stop, stepCount will get stuck in whatever it was before
+    if(!reset) begin 
         if(oldSecFlag == secFlag) begin
             stepCount <= stepCount + 1;
         end
@@ -33,31 +42,36 @@ end
 
 
 always @(posedge secClk or posedge reset) begin
+    secFlag = ~secFlag;
     if (reset)  begin
         secCount <= 0;
         secs <= 0; 
-        lock <= 1;
+       // lock <= 1;
     end
     else    begin
-        lock <= 0;
-        secFlag = ~secFlag;
-        
-        if(stepCount < 64)  begin
-            secCount <= 0;
+        //lock <= 0;
+        if(pauseSec)    begin
+            secCount <= secCount;
             secs <= secs;
         end
-        
-        else    begin
-            if(secs < 60)   begin      
-                secCount <= secCount + 1;
-                if(secCount == 60)
-                    secs <= 60;
-                else
-                    secs <= secs;
-            end
-            else    begin
-                secs <= secs +1;
+        else    begin    
+            if(stepCount < 64)  begin
                 secCount <= 0;
+                secs <= secs;
+            end
+            
+            else    begin
+                if(secs < 60)   begin      
+                    secCount <= secCount + 1;
+                    if(secCount == 60)
+                        secs <= 60;
+                    else
+                        secs <= secs;
+                end
+                else    begin
+                    secs <= secs +1;
+                    secCount <= 0;
+                end
             end
         end
     end
