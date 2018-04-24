0dfdef3b47025ebbd5c733652691e8ddadfe9927
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 03:47:50 -0500

    Update pulseGen.v

--- Lab 6/pulseGen.v
+++ Lab 6/pulseGen.v
@@ -1,47 +1,48 @@
-module pulseGen(CLK, START, MODE, PULSE);
-input CLK, START;
-input [1:0] MODE;
-output PULSE;
+module pulseGen(clk, start, mode, pulse, reset);   
+input clk, start, reset;
+input [1:0] mode;
+output pulse;
 reg [27:0] slowEdge;
 reg [8:0] secCounter;
 reg [1:0] enable;
 reg done;
-wire slowClk, secClk, CLK, START, LED1, LED2;
+wire slowClk, secClk, clk, start;
 
 initial
 begin
     secCounter = 0;
     done = 0;
+  //  lock = 0;
 end
 
 
 
-assign PULSE = (enable[0] == 1) ? slowClk : 0;
+assign pulse = (enable[0] == 1) ? slowClk : 0;
     
-complexDivider C1   (CLK, slowClk, slowEdge, enable[0]);
-complexDivider C2   (CLK, secClk, 50000000, enable[1]);
+complexDivider C1   (clk, slowClk, slowEdge, enable[0]);
+complexDivider C2   (clk, secClk, 50000000, enable[1]);
 
 
-always @(posedge CLK)
+always @(posedge clk)
 begin
-    if(!START) begin
+    if(!start || reset) begin
         enable <= 0;
         done <= 0;
     end
     else begin
-        case(MODE)
+        case(mode)
             0:  begin                       //walk
-                    slowEdge <= 1562500;
+                    slowEdge <= 1520000;
                     enable <= 2'b01;
                     done <= 0;
                 end
             1:  begin                       //jog
-                    slowEdge <= 781250;
+                    slowEdge <= 775193;
                     enable <= 2'b01;
                     done <= 0;
                 end 
             2:  begin                       //run
-                    slowEdge <= 390625;
+                    slowEdge <= 389205;
                     enable <= 2'b01;
                     done <= 0;
                 end
@@ -50,18 +51,18 @@ begin
                     begin
                         enable <= 2'b11;
                         case(secCounter)
-                        0:  slowEdge <= 2500000;
-                        1:  slowEdge <= 1515151;
-                        2:  slowEdge <= 757575;
-                        3:  slowEdge <= 1851851;
-                        4:  slowEdge <= 714285;
-                        5:  slowEdge <= 1666666;
-                        6:  slowEdge <= 2631579;
-                        7:  slowEdge <= 1666666;
-                        8:  slowEdge <= 1515151;
-                        9:  slowEdge <= 724638;
-                        73: slowEdge <= 1470588;
-                        78: slowEdge <= 403226;
+                        0:  slowEdge = 2439024;
+                        1:  slowEdge = 1492537;
+                        2:  slowEdge = 751879;
+                        3:  slowEdge = 1818181;
+                        4:  slowEdge = 709219;
+                        5:  slowEdge = 1639344;
+                        6:  slowEdge = 2564102;
+                        7:  slowEdge = 1639344;
+                        8:  slowEdge = 1492537;
+                        9:  slowEdge = 719424;
+                        73: slowEdge = 1449275;
+                        78: slowEdge = 401606;
                         default: slowEdge <= slowEdge;
                         endcase
                     end
@@ -78,12 +79,16 @@ begin
     end   
 end
 
-always @(posedge secClk)
+always @(posedge secClk or posedge reset)
 begin
-    if(!done) 
-        secCounter <= secCounter + 1; //as long as secClk is enabled, increment
-    else 
-        secCounter <= 0;
+    if (reset)
+        secCounter = 0;
+    else begin
+        if(!done) 
+            secCounter <= secCounter + 1; //as long as secClk is enabled, increment
+        else 
+            secCounter <= 0;
+    end
 end
 
 endmodule
