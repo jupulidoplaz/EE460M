bf301db2d680a2ed2a309bfeaf63fff6f514c708
jupulidoplaz <31829935+jupulidoplaz@users.noreply.github.com>
2018-04-24 03:49:23 -0500

    Update step32.v

--- Lab 6/step32.v
+++ Lab 6/step32.v
@@ -1,58 +1,54 @@
-module step32(CLK, steps, secClk, secs, reset);
-input CLK, secClk, steps, reset;
-output [3:0] secs;
-reg [20:0] totalSteps; 
-reg [3:0] secCount, oldSecCount, secs;
-reg resFlag;
-reg [6:0] stepCount;
+module step32(CLK, pulse, secClk, secs, reset); //pulse is the pulse signal
+input CLK, secClk, pulse, reset; 
+output reg [3:0] secs;
+reg [3:0] secCount;
+reg lock, secFlag, oldSecFlag;
+reg [7:0] stepCount;
 initial
 begin
-    stepCount = 0;
-    secCount = 0;
-    oldSecCount = 0;
+    lock = 0;
+    secFlag = 0;
+    oldSecFlag = 0;
     secs = 0;
-    resFlag = 0;
+    secCount = 0;
 end
     
-always @(posedge secClk or posedge reset or negedge reset)
+always@(posedge pulse) 
 begin
-    if(reset)
-    begin
-        secCount <= 0;
-        resFlag <= 1;   //means reset is high
-    end    
-    else begin
-        resFlag <= 0;   //means reset is low
-        if (secCount <= 9)  begin
-            secCount = secCount + 1;
-            if(stepCount >= 32)
-                secs = secs + 1;
-            else secs = secs;
-        end
-        else begin
-            secCount = 10;
-            secs = secs;
-        end
-    end
-end
-    
-always@(posedge steps)
-begin
-    if (secCount <= 9 && !resFlag) begin
-        if(secCount != oldSecCount) 
-            stepCount = 1;
-
+    if (!lock)  begin
+        if(oldSecFlag == secFlag)  
+            stepCount <= stepCount + 1;
         else
-	begin
-	    stepCount = stepCount + 1;
-            oldSecCount <= secCount;
-	end
+            stepCount <= 1;
+        oldSecFlag <= secFlag;
     end
-    else begin
-        stepCount = stepCount;
-        oldSecCount = oldSecCount;
-    end
-        
+    else    
+        stepCount <= 0;
 end
     
+always @(posedge secClk or posedge reset)
+begin
+    if (reset)  begin
+        lock <= 1;
+        secFlag <= secFlag;
+        secCount <= 0;
+        secs <= 0;
+    end
+    else    begin
+        lock <= 0;
+        secFlag = ~secFlag;
+        secCount = secCount + 1;
+        if (secCount <= 9)  begin    
+            if(stepCount >= 32)
+                secs <= secs + 1;
+            else
+                secs <= secs;
+        end
+        else    begin
+            secs <= secs;
+            secCount <= 10;
+        end         
+    end
+end
+
 endmodule
